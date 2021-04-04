ad0a0a8bc17cd6d9d6ffe3345ed9c393e42ef72e
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-04-04 01:14:53 -0300

    Links dos places e estilização

---
+++ src/templates/Places/index.tsx
@@ -0,0 +1,49 @@
+import LinkWrapper from 'components/LinkWrapper'
+import { CloseOutline } from '@styled-icons/evaicons-outline/CloseOutline'
+
+import * as S from './styles'
+
+type ImageProps = {
+  url: string
+  height: number
+  width: number
+}
+
+export type PlacesTemplateProps = {
+  place: {
+    slug: string
+    name: string
+    description: {
+      html: string
+    }
+    gallery: ImageProps[]
+  }
+}
+
+export default function PlacesTemplate({ place }: PlacesTemplateProps) {
+  return (
+    <>
+      <LinkWrapper href="/">
+        <CloseOutline size={32} aria-label="Go back to map" />
+      </LinkWrapper>
+
+      <S.Wrapper>
+        <S.Container>
+          <S.Heading>
+            <h1>{place.name}</h1>
+          </S.Heading>
+
+          <S.Body>
+            <div dangerouslySetInnerHTML={{ __html: place.description.html }} />
+          </S.Body>
+
+          <S.Gallery>
+            {place.gallery.map((image, index) => (
+              <img key={`photo-${index}`} src={image.url} alt={place.name} />
+            ))}
+          </S.Gallery>
+        </S.Container>
+      </S.Wrapper>
+    </>
+  )
+}

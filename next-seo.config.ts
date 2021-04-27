7a702e3e0443a8d0bb5b0a3d1c3433c44cfb1902
Ezequiel Brilhante <ezequielrb7@gmail.com>
2021-04-27 01:15:29 -0300

    Configurações de SEO

---
+++ next-seo.config.ts
@@ -0,0 +1,13 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://my-trips.ezequielbrilhante.com.br',
+    site_name: 'My Trips',
+  },
+  twitter: {
+    handle: '@EBrilhante7',
+    site: '@site',
+    cardType: 'summary_large_image',
+  },
+};
